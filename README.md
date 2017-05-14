008c876c93bb7e35940ce2cd048e8e113e24faa3
tanjoin <tanjoin@users.noreply.github.com>
2017-05-15 03:29:11 +0900

    チェックボックス

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # everyday-tasks
 毎日やることリスト
+## References
+- http://xn--u8je9fg7g9a9gp124a.com/weighttraining-abs/#i-2
+- http://xn--u8je9fg7g9a9gp124a.com/hometraining-lowerbody/
+- http://xn--u8je9fg7g9a9gp124a.com/hometrainingu-upperbody/
---
+++ js/index.js
@@ -0,0 +1,50 @@
+var KEY_TIME = "key_time";
+
+window.onload = function() {
+  if (!window.localStorage) {
+    alert("お使いのブラウザはlocalStorageに対応していません.");
+    return;
+  }
+
+  var dt = new Date(Date.parse(JSON.parse(getItem(KEY_TIME))));
+  if (dt) {
+    var limit = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + 1, 4, 0);
+    var now = new Date();
+
+    if (limit.getTime() < now.getTime()) {
+      clear();
+      return;
+    }
+  }
+
+  var inputs = document.getElementsByTagName("input");
+  for (var i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    var value = getItem(input.id);
+    if (value) {
+      input.checked = value;
+    }
+    input.onclick = clickCheckbox;
+  }
+};
+
+var clickCheckbox = function() {
+  saveItem(this.id, this.checked);
+  saveItem(KEY_TIME, JSON.stringify(new Date()));
+};
+
+var saveItem = function(key, val) {
+  window.localStorage.setItem(key, val);
+};
+
+var getItem = function(key) {
+  return window.localStorage.getItem(key);
+};
+
+var removeItem = function(key) {
+  window.localStorage.removeItem(key);
+};
+
+var clear = function() {
+  window.localStorage.clear();
+};
